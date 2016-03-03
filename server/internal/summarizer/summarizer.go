02e74c63e0f57636aa4072e3207122b16e6459f3
aLima89 <309020643+aLima89@users.noreply.github.com>
2016-03-03 14:23:00 +0200

    feat: embeddings

--- server/internal/summarizer/summarizer.go
+++ server/internal/summarizer/summarizer.go
@@ -5,3 +5,8 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/FjgarciaMac/AIrecall/server/internal/storage"
+)
+
+// Extractor finds "key: value" style facts in episode text.
+var factPattern = regexp.MustCompile(
