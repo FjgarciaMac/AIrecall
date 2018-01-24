e0a2d93be03193e134f946494fa645192bf66adf
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2018-01-24 18:46:00 +0200

    perf: improve summarization

--- server/internal/retrieval/retrieval.go
+++ server/internal/retrieval/retrieval.go
@@ -11,3 +11,13 @@ import (
 // Memory is the retrieval unit (mirrors storage.Episode).
 type Memory struct {
 	Content string
+}
+
+// Scorer scores one query against one memory.
+type Scorer struct{}
+
+// NewScorer returns a default scorer.
+func NewScorer() *Scorer { return &Scorer{} }
+
+// Hybrid ranks memories by combined keyword + vector score.
+// mode: "keyword" | "vector" | "hybrid"
