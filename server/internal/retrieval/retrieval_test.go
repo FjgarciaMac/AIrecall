2847b84231775d7401553b20bc302b36170a3948
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2018-02-21 11:00:00 +0200

    chore: agents

--- server/internal/retrieval/retrieval_test.go
+++ server/internal/retrieval/retrieval_test.go
@@ -3,3 +3,8 @@ package retrieval
 import "testing"
 
 func TestKeywordScoreExact(t *testing.T) {
+	s := NewScorer()
+	sc := s.Score("refund policy", "User asked about refund policy", "keyword")
+	if sc != 1.0 {
+		t.Fatalf("expected 1.0, got %f", sc)
+	}
