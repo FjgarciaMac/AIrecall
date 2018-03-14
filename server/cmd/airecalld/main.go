908ad4a10d022dac38c43abf42c2b91d90f449bd
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2018-03-14 15:39:00 +0200

    test: cover summarization

--- server/cmd/airecalld/main.go
+++ server/cmd/airecalld/main.go
@@ -1,3 +1,8 @@
 // Command airecalld runs the AIrecall memory server.
 //
 // A small JSON API over the same SQLite schema the SDK uses in dev
+// mode, plus hybrid retrieval and the summarization pass.
+package main
+
+import (
+	"flag"
