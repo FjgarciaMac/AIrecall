ed05b622d9f03ecaa8de3a648f7d64d9d1563154
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2019-01-01 13:37:00 +0200

    fix: correct golang

--- server/internal/api/api.go
+++ server/internal/api/api.go
@@ -15,3 +15,17 @@ type Config struct {
 	TopK        int    `json:"top_k"`
 	Mode        string `json:"mode"` // keyword | vector | hybrid
 	RecallLimit int    `json:"recall_limit"`
+}
+
+// DefaultConfig returns the built-in defaults.
+func DefaultConfig() Config {
+	return Config{TopK: 5, Mode: "hybrid", RecallLimit: 200}
+}
+
+// Server holds the handlers.
+type Server struct {
+	store      *storage.Store
+	scorer     *retrieval.Scorer
+	summarizer *summarizer.Summarizer
+	cfg        Config
+}
