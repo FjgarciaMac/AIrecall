2c8b714612794b7cdbd9cb672d64cff1b390492d
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2019-01-03 15:11:00 +0200

    test: cover retrieval

--- server/internal/api/api.go
+++ server/internal/api/api.go
@@ -71,3 +71,17 @@ func (s *Server) handleRemember(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "content required", http.StatusBadRequest)
 		return
 	}
+	if err := s.store.AddEpisode(agent, content); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	writeJSON(w, map[string]any{"ok": true})
+}
+
+func (s *Server) handleUpsertFact(w http.ResponseWriter, r *http.Request) {
+	body, ok := s.decode(w, r)
+	if !ok {
+		return
+	}
+	agent := str(body, "agent_id", "default")
+	key := str(body, "key", "")
