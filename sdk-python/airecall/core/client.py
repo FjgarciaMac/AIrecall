daf011415d80063c2fdf0c0ccb4925172d1a9364
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2020-01-22 09:47:00 +0200

    style: clean up embeddings

--- sdk-python/airecall/core/client.py
+++ sdk-python/airecall/core/client.py
@@ -3,3 +3,10 @@
 A thin HTTP client. The server exposes a small JSON API:
 
   POST /v1/remember          {"agent_id", "content"}
+  POST /v1/facts             {"agent_id", "key", "value"}
+  POST /v1/recall            {"agent_id", "query", "top_k"}
+  GET  /v1/facts/{agent}/{key}
+  POST /v1/summarize         {"agent_id"}
+"""
+
+from __future__ import annotations
