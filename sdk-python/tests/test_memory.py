91fa5a9ba54f7ea56cf99e2bf2b369e7d7426fa2
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2022-01-04 13:15:00 +0200

    docs: document sdk

--- sdk-python/tests/test_memory.py
+++ sdk-python/tests/test_memory.py
@@ -11,3 +11,10 @@ from airecall.core.memory import Memory
 @pytest.fixture()
 def memory(tmp_path):
     m = Memory(agent_id="test-agent", db_path=str(tmp_path / "m.db"))
+    yield m
+    m.close()
+
+
+def test_remember_and_recall(memory):
+    memory.remember("User asked about refund policy for ORD-9921")
+    memory.remember("User prefers email over phone")
